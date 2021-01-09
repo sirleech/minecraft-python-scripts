03de853110c83757ef0aa5dc1a768811465a0d14
Chris Lee <sirleech@gmail.com>
2021-01-09 16:17:54 +1100

    better tree

--- tree.py
+++ tree.py
@@ -2,14 +2,16 @@ from mcpi.minecraft import Minecraft
 
 mc = Minecraft.create()
 
-# Chris = mc.getPlayerEntityId("SonarNeedle")
-# x, y, z = mc.entity.getPos(Chris)
-x, y, z = mc.player.getPos()
+SonarNeedle = mc.getPlayerEntityId("SonarNeedle")
+sirleech = mc.getPlayerEntityId("sirleech")
+x, y, z = mc.entity.getPos(SonarNeedle)
 
 #canopy
-size = 25
-mc.setBlocks(x+1, y+1, z+1, x+size, y+size/2, z+size, 18)
+size = 10
+#offset
+off = 2
+mc.setBlocks(x + off, y, z+ off, x+size, y+size/2, z+size, 18)
 
 #trunk
-size = 3
-mc.setBlocks(x+size, y, z+size, x-size, y-size*30, z-size, 17)
+mid = size/2
+mc.setBlocks(x+off+mid , y, z+off+mid, x+mid+off+2, y-50, z+mid+off+2, 17)
