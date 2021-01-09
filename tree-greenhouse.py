081ab93284072a3d11cd3a890326802fd79cf7ef
Chris Lee <sirleech@gmail.com>
2021-01-09 19:57:06 +1100

    Updates

--- tree-greenhouse.py
+++ tree-greenhouse.py
@@ -12,9 +12,17 @@ size = 20
 
 #offset from player
 off = 2
-mc.setBlocks(x + off, y, z+ off, x+size, y+size/2, z+size, 18)
+#glass
+mc.setBlocks(x + off, y, z+ off, x+size, y+size/2, z+size, 20)
+#air
+mc.setBlocks(x+off+1, y+1, z+off+1, x+size-1, y+(size/2)-1, z+size-1, 0)
+#grass=2
+mc.setBlocks(x+off+1, y+1, z+off+1, x+size-1, y+1, z+size-1, 2)
+#saplings
+mc.setBlocks(x+off+1, y+2, z+off+1, x+size-1, y+2, z+size-1, 6)
+
 
 #fat trunk
-trunk = 12
+trunk = 14
 mid = size/2
 mc.setBlocks(x+off+mid-(trunk/2),y,z+off+mid-(trunk/2),x+off+trunk,y-50,z+off+trunk, 17)
