ab4fdbb8a2537dc898b772b4481229482b063641
Chris Lee <sirleech@gmail.com>
2021-01-09 19:43:27 +1100

    trees in glass

--- tree-in-glass.py
+++ tree-in-glass.py
@@ -5,16 +5,19 @@ mc = Minecraft.create()
 SonarNeedle = mc.getPlayerEntityId("SonarNeedle")
 sirleech = mc.getPlayerEntityId("sirleech")
 # replace with your player entity ID
-x, y, z = mc.entity.getPos(sirleech)
+x, y, z = mc.entity.getPos(SonarNeedle)
 
 #canopy size
 size = 20
 
 #offset from player
 off = 2
-mc.setBlocks(x + off, y, z+ off, x+size, y+size/2, z+size, 18)
+#glass
+mc.setBlocks(x + off, y, z+ off, x+size, y+size/2, z+size, 20)
+#foliage = 18
+mc.setBlocks(x+off+1, y+1, z+off+1, x+size-1, y+(size/2)-1, z+size-1, 18)
 
 #fat trunk
-trunk = 18
+trunk = 14
 mid = size/2
 mc.setBlocks(x+off+mid-(trunk/2),y,z+off+mid-(trunk/2),x+off+trunk,y-50,z+off+trunk, 17)
